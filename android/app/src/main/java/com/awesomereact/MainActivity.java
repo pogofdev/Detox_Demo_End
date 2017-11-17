47089a7348c67e05c4d494eb1363615a1a9a3968
poisongas <poisongass@hotmail.com>
2017-11-17 11:10:25 +0700

    react native navigation runs on android

--- android/app/src/main/java/com/awesomereact/MainActivity.java
+++ android/app/src/main/java/com/awesomereact/MainActivity.java
@@ -1,15 +1,29 @@
 package com.awesomereact;
 
-import com.facebook.react.ReactActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.ImageView;
+import android.view.Gravity;
+import android.util.TypedValue;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
+        public class MainActivity extends SplashActivity {
+                @Override
+                public LinearLayout createSplashLayout() {
+                        LinearLayout view = new LinearLayout(this);
+                        ImageView imageView = new ImageView(this);
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "AwesomeReact";
-    }
-}
+                        view.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                        view.setGravity(Gravity.CENTER);
+
+                        // hard code the width and the height of the logo
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(400, 167);
+                        layoutParams.gravity = Gravity.CENTER;
+                        imageView.setLayoutParams(layoutParams);
+                        imageView.setImageDrawable(ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.splashscreen));
+
+                        view.addView(imageView);
+                        return view;
+                }
+        }
